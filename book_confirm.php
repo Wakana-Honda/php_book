74fc19f9ebc78881c8f01081bc9293d0d1ca3164
EC2 Default User <ec2-user@ip-172-31-30-70.ap-northeast-1.compute.internal>
2023-02-07 08:46:53 +0000

    HTMLにDB記入

--- book_confirm.php
+++ book_confirm.php
@@ -1,6 +1,43 @@
 <?php
  $inputTitle = $_POST["inputtitle"];
  $inputText = $_POST["inputtext"];
+
+
+#入力データチェック
+if(empty($inputTitle)){
+ echo "タイトルを入力してください";
+ exit;
+}
+
+#データ登録
+$db['host'] =
+$db['user'] =
+$db['pass'] =
+$db['dbname'] =
+$dsn = sprintf('mysql:host=%s;dbname=%s;charset=utf8',$db['host'],$db['dbname']);
+ try{
+  $hdb = new POD($dsn,$db['user'],$db['pass'],array(POD::ATTR_ERRMODE=>POD::ERROMODE_EXCEPTION));
+ }catch(PDOException $e){
+  exit("Database Connection Error." .$e->getMessage());
+ }
+ $sql = "insert into blog(title,
+                          text,
+                          updatetime
+                         )values(
+                         :v_title,
+                         :v_text,
+                         now()
+                          )";
+ $stmt = $hdb -> prepare($sql);
+ $stmt -> bindparam("v_title",$inputTitle);
+ $stmt -> bindparam("v_text",$inputText);
+ $stmt -> execute();
+ if (!$stmt){
+  $info = $stmt -> errorInfo();
+  exit($info[2]);
+ }
+ 
+
 ?>
 
 <!DOCTYPE html>
