a272b425cc92631caf3d70a65f14d9aa557dd56f
EC2 Default User <ec2-user@ip-172-31-30-70.ap-northeast-1.compute.internal>
2023-02-09 09:54:35 +0000

    DB断念

--- book_confirm.php
+++ book_confirm.php
@@ -9,33 +9,33 @@ if(empty($inputTitle)){
  exit;
 }
 
-#データ登録
-$db['host'] =
-$db['user'] =
-$db['pass'] =
-$db['dbname'] =
-$dsn = sprintf('mysql:host=%s;dbname=%s;charset=utf8',$db['host'],$db['dbname']);
- try{
-  $hdb = new POD($dsn,$db['user'],$db['pass'],array(POD::ATTR_ERRMODE=>POD::ERROMODE_EXCEPTION));
- }catch(PDOException $e){
-  exit("Database Connection Error." .$e->getMessage());
- }
- $sql = "insert into blog(title,
-                          text,
-                          updatetime
-                         )values(
-                         :v_title,
-                         :v_text,
-                         now()
-                          )";
- $stmt = $hdb -> prepare($sql);
- $stmt -> bindparam("v_title",$inputTitle);
- $stmt -> bindparam("v_text",$inputText);
- $stmt -> execute();
- if (!$stmt){
-  $info = $stmt -> errorInfo();
-  exit($info[2]);
- }
+// #データ登録
+// $db['host'] =
+// $db['user'] =
+// $db['pass'] =
+// $db['dbname'] =
+// $dsn = sprintf('mysql:host=%s;dbname=%s;charset=utf8',$db['host'],$db['dbname']);
+//  try{
+//   $hdb = new POD($dsn,$db['user'],$db['pass'],array(POD::ATTR_ERRMODE=>POD::ERROMODE_EXCEPTION));
+//  }catch(PDOException $e){
+//   exit("Database Connection Error." .$e->getMessage());
+//  }
+//  $sql = "insert into blog(title,
+//                           text,
+//                           updatetime
+//                          )values(
+//                          :v_title,
+//                          :v_text,
+//                          now()
+//                           )";
+//  $stmt = $hdb -> prepare($sql);
+//  $stmt -> bindparam("v_title",$inputTitle);
+//  $stmt -> bindparam("v_text",$inputText);
+//  $stmt -> execute();
+//  if (!$stmt){
+//   $info = $stmt -> errorInfo();
+//   exit($info[2]);
+//  }
  
 
 ?>
